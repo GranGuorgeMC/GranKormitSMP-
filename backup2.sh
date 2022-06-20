9a7c6c37906d93d7a87be6c1d1d3fbfaddc42c80
GranGuorge <90460977+GranGuorgeMC@users.noreply.github.com>
2022-06-20 08:30:20 +0800

    Delete backup2.sh

--- backup2.sh
+++
@@ -1,6 +0,0 @@
-wget https://raw.githubusercontent.com/GranGuorgeMC/GranKormitSMP-/main/backup.now
-chmod +x backup.now
-wget https://raw.githubusercontent.com/GranGuorgeMC/GranKormitSMP-/main/backup.2
-chmod +x backup.2
-wget https://raw.githubusercontent.com/GranGuorgeMC/GranKormitSMP-/main/backup.3
-chmod +x backup.3
